f32cbd4bc26e658106796e4f16e5f24c817b5375
Richard Tinajero <rj.tinajero92@gmail.com>
2019-02-27 12:00:51 -0800

    checkpoint commit

---
+++ bash.js
@@ -0,0 +1,12 @@
+const commands = require("./commands.js");
+
+//prompt the user for input
+process.stdout.write('prompt > ');
+
+//The stdin 'data' event triggers after a user types ion a line
+process.stdin.on('data', (userInput) => {
+   userInput = userInput.toString().trim();
+    //evaluateCmd is a function which will be implemented in commands.js
+   commands.evaluateCmd(userInput);
+});
+
